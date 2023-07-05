ea358257f4f6ce8cd64fba1c7eae98cd44240971
Krishanu Goswami <krishgoswami15@gmail.com>
2023-07-05 21:21:44 +0530

    moved to 02-medium

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+// import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 /// File is incomplete. You need to add input boxes to take input for users to login.
 function Login() {
     const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    // const history = useHistory();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post("http://localhost:3000/admin/login");
+            const { token } = response.data;
+
+            localStorage.setItem('token', token);
+
+            // history.push('/');
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
     return <div>
         <h1>Login to admin dashboard</h1>
-        <br/>
-        Email - <input type={"text"} onChange={e => setEmail(e.target.value)} />
-        <br/>
-        <button>Login</button>
-        <br/>
-        New here? <a href="/register">Register</a>
+        <form onSubmit={handleLogin}>
+            <br />
+            <label>Email:</label>
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} /> <br />
+            <label>Password:</label>
+            <input type={"password"} value={password} onChange={e => setPassword(e.target.value)} />
+            <br />
+            <button type="submit">Login</button>
+            <br />
+            New here? <a href="/register">Register</a>
+        </form>
     </div>
 }
 
--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
@@ -3,12 +3,15 @@ import React from "react";
 /// File is incomplete. You need to add input boxes to take input for users to register.
 function Register() {
     const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
     return <div>
         <h1>Register to the website</h1>
-        <br/>
-        <input type={"text"} onChange={e => setEmail(e.target.value)} />
-        <br/>
+        <br />
+        Email - <input type={"email"} onChange={e => setEmail(e.target.value)} /> <br />
+        Password - <input type={"password"} onChange={e => setPassword(e.target.value)} />
+        <br />
+        <button>Sign up</button>
         Already a user? <a href="/login">Login</a>
     </div>
 }
