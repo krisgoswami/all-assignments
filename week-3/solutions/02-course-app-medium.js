552bd8f84969d9d6c39b6dba597d0f9ace77c8e0
Krishanu <krishgoswami15@gmail.com>
2023-07-11 20:41:04 +0530

    added new route

--- week-3/solutions/02-course-app-medium.js
+++ week-3/solutions/02-course-app-medium.js
@@ -36,6 +36,12 @@ const authenticateJwt = (req, res, next) => {
   }
 };
 
+app.get('/admin/me', authenticateJwt, (req, res) => {
+  res.json({
+    username: req.user.username
+  })
+})
+
 app.post('/admin/signup', (req, res) => {
   const admin = req.body;
   const existingAdmin = ADMINS.find(a => a.username === admin.username);
@@ -62,7 +68,7 @@ app.post('/admin/login', (req, res) => {
 
 app.post('/admin/courses', authenticateJwt, (req, res) => {
   const course = req.body;
-  course.id = COURSES.length + 1; 
+  course.id = COURSES.length + 1;
   COURSES.push(course);
   res.json({ message: 'Course created successfully', courseId: course.id });
 });
